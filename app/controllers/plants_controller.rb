7b5be1707ef136e7eff953829ff4524ceeb4bda1
Levi Barsis <lbarsis@gmail.com>
2023-01-28 15:43:41 -0800

    Done with assignment

--- app/controllers/plants_controller.rb
+++ app/controllers/plants_controller.rb
@@ -1,4 +1,5 @@
 class PlantsController < ApplicationController
+rescue_from ActiveRecord::RecordNotFound, with: :record_not_found_response
 
   # GET /plants
   def index
@@ -8,7 +9,7 @@ class PlantsController < ApplicationController
 
   # GET /plants/:id
   def show
-    plant = Plant.find_by(id: params[:id])
+    plant = find_plant
     render json: plant
   end
 
@@ -18,9 +19,31 @@ class PlantsController < ApplicationController
     render json: plant, status: :created
   end
 
+  # PATCH /plants/:id
+  def update
+    plant = find_plant
+    plant.update(plant_params)
+    render json: plant, status: :accepted
+  end
+
+  # DELETE /plants/:id
+  def destroy
+    plant = find_plant
+    plant.destroy
+    head :no_content
+  end
+
   private
+
+  def find_plant
+    Plant.find(params[:id])
+  end
 
   def plant_params
     params.permit(:name, :image, :price, :is_in_stock)
+  end
+
+  def record_not_found_response
+    render json: {message: 'Plant not Found'}, status: :not_found
   end
 end
